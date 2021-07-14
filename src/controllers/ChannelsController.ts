7be6169176b769089a88f599d348b416a9413d35
diegogaitanmerkas <57677755+diegogaitanmerkas@users.noreply.github.com>
2021-07-14 16:14:33 -0500

    make it better

--- src/controllers/ChannelsController.ts
+++ src/controllers/ChannelsController.ts
@@ -8,9 +8,48 @@ class ChannelsController {
     async getData(request: Request, response: Response) {
 
         const plutoTvClient = new PlutoTvClient();
-        const data = await plutoTvClient.getData();
+        const channels = await plutoTvClient.getData();
 
-        return response.json(data);
+        channels.forEach((channel, index) => {
+            let deviceId = uuid1();
+            let sid = uuid4();
+
+            if (channel.isStitched) {
+                const m3uUrl = new URL(channel.stitched.urls[0].url);
+                let queryString = m3uUrl.search;
+                let params = new URLSearchParams(queryString);
+
+                // set the url params
+                params.set("advertisingId", "");
+                params.set("appName", "web");
+                params.set("appVersion", "unknown");
+                params.set("appStoreUrl", "");
+                params.set("architecture", "");
+                params.set("buildVersion", "");
+                params.set("clientTime", "0");
+                params.set("deviceDNT", "0");
+                params.set("deviceId", deviceId);
+                params.set("deviceMake", "Chrome");
+                params.set("deviceModel", "web");
+                params.set("deviceType", "web");
+                params.set("deviceVersion", "unknown");
+                params.set("includeExtendedEvents", "false");
+                params.set("sid", sid);
+                params.set("userId", "");
+                params.set("serverSideAds", "true");
+
+                m3uUrl.search = params.toString();
+                const channelUrl = m3uUrl.toString();
+
+                channels[index]['stitched']['url'] = channelUrl
+
+                console.debug("[DEBUG] Adding " + channel.name + " channel.");
+            } else {
+                console.debug("[DEBUG] Skipping 'fake' channel " + channel.name + ".");
+            }
+        });
+        console.log(channels)
+        return response.json(channels);
     }
 
     async getChannels(request: Request, response: Response) {
@@ -55,7 +94,7 @@ class ChannelsController {
                 const group = channel.category;
                 const name = channel.name;
                 const number = channel.number;
-
+                console.log(channelUrl)
                 m3u8 +=
                     `#EXTINF:-1 channel-id="${slug}" tvg-chno="${number}" tvg-logo="${logo}" group-title="${group}", ${name}` +
                     "\n";
